8fb8c63b597a7f26b37acf60fdd12ba7524f7370
xhyrom <generalkubo@gmail.com>
2022-04-15 20:34:32 +0200

    base

--- _config.ts
+++ _config.ts
@@ -1,23 +1,24 @@
-import lume from "https://deno.land/x/lume@v1.7.3/mod.ts";
-import postcss from "https://deno.land/x/lume@v1.7.3/plugins/postcss.ts";
-import sass from "https://deno.land/x/lume@v1.7.3/plugins/sass.ts";
-import esbuild from "https://deno.land/x/lume@v1.7.3/plugins/esbuild.ts";
-import codeHighlight from "https://deno.land/x/lume@v1.7.3/plugins/code_highlight.ts";
+import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
+import sass from "lume/plugins/sass.ts";
+import esbuild from "lume/plugins/esbuild.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
 import { htmlMinifier } from "https://raw.githubusercontent.com/xHyroM/xHyroM/master/packages/site/plugins/htmlMinifier.ts";
+import resolveUrls from "lume/plugins/resolve_urls.ts";
 
 const site = lume(
-    {
-      src: "./src",
-      server: {
-        page404: "./404/",
-      },
+  {
+    src: "./src",
+    server: {
+      page404: "./404/",
     },
+  },
 );
 
 site
   .ignore("README.md")
   .copy("static", ".")
-  .loadAssets([".ts"])
+  .use(resolveUrls())
   .use(htmlMinifier())
   .use(sass())
   .use(codeHighlight())
@@ -38,4 +39,5 @@ site
   )
   // Filters
   .filter("slice", (arr, length) => arr.slice(0, length));
+
 export default site;
